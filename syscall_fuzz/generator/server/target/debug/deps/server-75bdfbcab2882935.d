f798fc2348124e5c6e07957a326f0ff8c288a08a
lvboqizw <925220146@qq.com>
2023-04-14 16:52:29 +0200

    update .gitignore

--- syscall_fuzz/generator/server/target/debug/deps/server-75bdfbcab2882935.d
+++
@@ -1,5 +0,0 @@
-/home/wei/Documents/Studienarbeit/syscall_fuzz/generator/server/target/debug/deps/server-75bdfbcab2882935.rmeta: src/main.rs
-
-/home/wei/Documents/Studienarbeit/syscall_fuzz/generator/server/target/debug/deps/server-75bdfbcab2882935.d: src/main.rs
-
-src/main.rs:
--- syscall_fuzz/generator/server/target/debug/deps/server-e4ecbd1924a0b864.d
+++
@@ -1,5 +0,0 @@
-/home/wei/Documents/Studienarbeit/syscall_fuzz/generator/server/target/debug/deps/server-e4ecbd1924a0b864.rmeta: src/main.rs
-
-/home/wei/Documents/Studienarbeit/syscall_fuzz/generator/server/target/debug/deps/server-e4ecbd1924a0b864.d: src/main.rs
-
-src/main.rs:
