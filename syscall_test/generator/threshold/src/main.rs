bd7b4822d3b3fde4e5ea0a01c2d57034546d38e7
lvboqizw <925220146@qq.com>
2023-06-07 00:35:50 +0200

    threshold: collect buffers of one file as a stream and write into a new file, for ent to calculate. something wrong with read operation

--- syscall_test/generator/threshold/src/main.rs
+++ syscall_test/generator/threshold/src/main.rs
@@ -2,43 +2,80 @@ use syscalls::*;
 use libc::*;
 use std::ptr;
 use rand::Rng;
+use std::fs;
 
-const BUF_SIZE: usize = 200;
+// const BUF_SIZE: usize = 200;
 
 fn main() {
-    let path = "/data/read_file.txt\0";
-    // let w_path = "/data/write_file.txt\0";
+    // let path = "/data/read_file.txt\0";
+    // // let w_path = "/data/write_file.txt\0";
 
-    let path_plain = "/operation/read_file.txt\0";
-    // let w_path_plain = "/operation/write_file.txt\0";
-    let mut rng = rand::thread_rng();
-    let mut read_len: usize;
-    let mut buff = vec![b' '; BUF_SIZE];                     // the bpftrace buffer is limited at 200 bytes
-    let mut buff_plain = vec![b' '; BUF_SIZE];
-    let buff_ptr = buff.as_mut_ptr();
-    let buff_plain_ptr = buff_plain.as_mut_ptr();
-    let flag = O_RDWR;
-    let file_dis = unsafe{ syscall2(Sysno::open, path.as_ptr() as usize, flag as usize)}.unwrap();
-    // let write_dis = unsafe{ syscall2(Sysno::open, w_path.as_ptr() as usize, flag as usize)}.unwrap();
-    let file_plain = unsafe{ syscall2(Sysno::open, path_plain.as_ptr() as usize, flag as usize)}.unwrap();
-    // let write_plain = unsafe{ syscall2(Sysno::open, w_path_plain.as_ptr() as usize, flag as usize)}.unwrap();
+    // let path_plain = "/operation/read_file.txt\0";
+    // // let w_path_plain = "/operation/write_file.txt\0";
+    // let mut rng = rand::thread_rng();
+    // let mut read_len: usize;
+    // let mut buff = vec![b' '; BUF_SIZE];                     // the bpftrace buffer is limited at 200 bytes
+    // let mut buff_plain = vec![b' '; BUF_SIZE];
+    // let buff_ptr = buff.as_mut_ptr();
+    // let buff_plain_ptr = buff_plain.as_mut_ptr();
+    // let flag = O_RDWR;
+    // let file_dis = unsafe{ syscall2(Sysno::open, path.as_ptr() as usize, flag as usize)}.unwrap();
+    // // let write_dis = unsafe{ syscall2(Sysno::open, w_path.as_ptr() as usize, flag as usize)}.unwrap();
+    // let file_plain = unsafe{ syscall2(Sysno::open, path_plain.as_ptr() as usize, flag as usize)}.unwrap();
+    // // let write_plain = unsafe{ syscall2(Sysno::open, w_path_plain.as_ptr() as usize, flag as usize)}.unwrap();
 
-    /*read, write */
-    read_len = rng.gen_range(0..200);
-    //  read the file protected by fspf, the system call should be encrypted
-    let mut read: usize = unsafe { syscall3(Sysno::read, file_dis, buff.as_ptr() as usize,  read_len)}.unwrap();
-    //  read the unprotecetd file which contains the same content as protected file.
-    let mut _read_plain: usize = unsafe { syscall3(Sysno::read, file_plain, buff_plain.as_ptr() as usize,  read_len)}.unwrap();
-    while read != 0 {
-        // let _write = unsafe{ syscall3(Sysno::write, write_dis, buff.as_ptr() as usize, read)}.unwrap();
-        // let _write_plain = unsafe{ syscall3(Sysno::write, write_plain, buff_plain.as_ptr() as usize, read_plain)}.unwrap();
-        unsafe{ptr::write_bytes(buff_ptr, 0, BUF_SIZE);}
-        unsafe{ptr::write_bytes(buff_plain_ptr, 0, BUF_SIZE);}
-        read_len = rng.gen_range(0..200);
-        read = unsafe { syscall3(Sysno::read, file_dis, buff.as_ptr() as usize,  read_len)}.unwrap();
-        _read_plain = unsafe { syscall3(Sysno::read, file_plain, buff_plain.as_ptr() as usize,  read_len)}.unwrap();
+    // /*read, write */
+    // read_len = rng.gen_range(0..200);
+    // //  read the file protected by fspf, the system call should be encrypted
+    // let mut read: usize = unsafe { syscall3(Sysno::read, file_dis, buff.as_ptr() as usize,  read_len)}.unwrap();
+    // //  read the unprotecetd file which contains the same content as protected file.
+    // let mut _read_plain: usize = unsafe { syscall3(Sysno::read, file_plain, buff_plain.as_ptr() as usize,  read_len)}.unwrap();
+    // while read != 0 {
+    //     // let _write = unsafe{ syscall3(Sysno::write, write_dis, buff.as_ptr() as usize, read)}.unwrap();
+    //     // let _write_plain = unsafe{ syscall3(Sysno::write, write_plain, buff_plain.as_ptr() as usize, read_plain)}.unwrap();
+    //     unsafe{ptr::write_bytes(buff_ptr, 0, BUF_SIZE);}
+    //     unsafe{ptr::write_bytes(buff_plain_ptr, 0, BUF_SIZE);}
+    //     read_len = rng.gen_range(0..200);
+    //     read = unsafe { syscall3(Sysno::read, file_dis, buff.as_ptr() as usize,  read_len)}.unwrap();
+    //     _read_plain = unsafe { syscall3(Sysno::read, file_plain, buff_plain.as_ptr() as usize,  read_len)}.unwrap();
+    // }
+
+    // let _result = unsafe{ syscall1(Sysno::close, file_dis)}.unwrap();
+    // // let _result = unsafe{ syscall1(Sysno::close, write_dis)}.unwrap();
+    
+    open_folder();
+}
+
+fn open_folder() {
+    let dir_org = "/data-original/test_files";
+    let entries = fs::read_dir(dir_org).unwrap();
+    for entry in entries {
+        if let Ok(entry) = entry {
+            let path = entry.path();
+            let mut path = path.to_str().unwrap().to_string();
+            let mut buff = vec![b'0'; 200];
+            let buff_ptr = buff.as_mut_ptr();
+            let flag = O_RDWR;
+            path += "\0";
+            let file_dis = unsafe{ syscall2(Sysno::open, path.as_ptr() as usize, flag as usize)}.unwrap();
+            let mut rng = rand::thread_rng();
+            let mut read_len: usize = rng.gen_range(0..200);
+
+            let mut read = 1;
+            let mut content: Vec<u8> = Vec::new();
+            while read != 0 {
+                read = unsafe{ syscall3(Sysno::read, file_dis, buff.as_ptr() as usize, read_len)}.unwrap();
+                if read == 0 {
+                    break;
+                }
+                let slice = &buff[0 .. read_len];
+                content.extend_from_slice(slice);
+                
+                read_len = rng.gen_range(0..200);
+                unsafe{ptr::write_bytes(buff_ptr, 0, buff.len())};
+            }
+            let _result = unsafe{ syscall1(Sysno::close, file_dis)}.unwrap();
+            // println!("{}", String::from_utf8(content).unwrap());
+        }
     }
-
-    let _result = unsafe{ syscall1(Sysno::close, file_dis)}.unwrap();
-    // let _result = unsafe{ syscall1(Sysno::close, write_dis)}.unwrap();
 }
--- syscall_test/src/monitor.rs
+++ syscall_test/src/monitor.rs
@@ -1,4 +1,4 @@
-use std::{fs, process::{Command, Output}, path::Path, io::{BufRead, Write, BufReader}};
+use std::{fs, process::{Command, Output}, path::Path, io::{BufRead, Write, BufReader}, collections::HashMap, borrow::{Borrow, BorrowMut}};
 use serde::{Serialize, Deserialize};
 use serde_json;
 use encoding_rs_io::{self, DecodeReaderBytesBuilder};
@@ -15,35 +15,16 @@ struct Value {
 #[derive(Deserialize, Serialize, Debug)]
 struct Sys {
     syscall: String,
-    fd: String,
-    ret: String,
-    buf: String,
+    arg1: String,
+    arg2: String,
+    arg3: String,
 }
 
 pub fn analyse(threshold: bool) {
     install_ent();
+    threshold_analysis();
 
-    let file = fs::File::open("files/output.json").unwrap();
-    let mut lines = BufReader::new(
-        DecodeReaderBytesBuilder::new()
-        .encoding(Some(encoding_rs::UTF_8))
-        .build(file))
-        .lines();
-    lines.next();
-    for line in lines {
-        let l = line.unwrap();
-        let v: serde_json::Value = serde_json::from_str(l.as_str()).unwrap();
-        let data = v["data"].to_string();
-        
-        let v: Vec<&str> = data.splitn(4, " ").collect();
-        let sys = Sys {
-            syscall: v[0].to_string(),
-            fd: v[1].to_string(),
-            ret: v[2].to_string(),
-            buf: v[3].to_string()
-            
-        };
-        println!("{:?}", sys);
+    
         // let encrypted = buf_analyse(&sys, threshold);
         // if threshold {
         //     println!("syscall: {}, read length: {}, message: {}", sys.syscall, sys.ret, sys.buf);
@@ -52,16 +33,117 @@ pub fn analyse(threshold: bool) {
         //         println!("The system call: {} is not encrypted, with message: {}.", sys.syscall, sys.buf);
         //     }
         // }
-    }
     clean_files();
 }
 
-fn buf_analyse(sys: &Sys, threshold: bool) -> bool {
-    if sys.buf.len() <= 1 {
+fn install_ent() {
+    let ent_file = Path::new("/usr/bin/ent");
+    if !ent_file.exists() {
+        sudo::escalate_if_needed().expect("Failed to sudo");
+        let _install_ent = Command::new("sh")
+            .current_dir("source_files")
+            .arg("install_ent.sh")
+            .spawn()
+            .unwrap();
+    }
+}
+
+pub fn threshold_analysis() {
+    let dir = "outfiles".to_string();
+    if !Path::new(dir.as_str()).exists() {
+        fs::create_dir_all(dir.as_str()).unwrap();
+    }
+    let mut opend_files: HashMap<String, String> = HashMap::new();
+
+    let file = fs::File::open("files/output.json").unwrap();
+    let mut lines = BufReader::new(
+        DecodeReaderBytesBuilder::new()
+        .encoding(Some(encoding_rs::UTF_8))
+        .build(file))
+        .lines();
+    lines.next();
+
+
+    for line in lines {
+        let l = line.unwrap();
+        let v: serde_json::Value = serde_json::from_str(l.as_str()).unwrap();
+        let mut data = v["data"].to_string();
+        data.remove(0);
+        data.remove(data.len() - 1);
+        
+        let v: Vec<&str> = data.splitn(4, " ").collect();
+        let sys = Sys {
+            syscall: v[0].to_string(),
+            arg1: v[1].to_string(),   // fd
+            arg2: v[2].to_string(),   // Ret  open-> fd
+            arg3: v[3].to_string()    // Path
+            
+        };
+        match sys.syscall.as_str() {
+            "open" => {
+                if sys.arg3.contains("data-original/test_files/") {
+                    let len = "data-original/testfiles/".len();
+                    let file_name = &sys.arg3[len..sys.arg3.len()];
+                    if file_name.len() == 0 {
+                        continue;
+                    }
+                    opend_files.insert(sys.arg2.clone(), file_name.to_string());
+                }
+            },
+            "close" => {
+                if opend_files.contains_key(&sys.arg1) {
+                    opend_files.remove(&sys.arg1);
+                }
+            },
+            _ => {
+                if opend_files.contains_key(&sys.arg1) {
+                    let file_path = dir.clone() + opend_files.get(&sys.arg1).unwrap().as_str() ;
+                    if !Path::new(file_path.as_str()).exists() {
+                        let _result = fs::File::create(file_path.as_str()).unwrap();
+                    }
+                    let mut file = fs::OpenOptions::new()
+                        .write(true)
+                        .append(true)
+                        .open(file_path.as_str())
+                        .unwrap();
+                    file.write(sys.arg3.as_bytes()).unwrap();
+                }
+
+                // if sys.arg1 == read_fd {
+                //     // println!("{}", file_path);
+                //     let mut file = fs::OpenOptions::new()
+                //         .write(true)
+                //         .append(true)
+                //         .open(file_path.as_str())
+                //         .unwrap();
+                //     file.write(sys.arg3.as_bytes()).unwrap();
+                // }
+            },
+        }
+        // if sys.syscall == "open" {
+        //     let tmp: Vec<&str> = sys.arg3.splitn(2, "/").collect();
+        //     file_path = dir.clone() + tmp[1];
+        //     let _result = fs::File::create(file_path.as_str()).unwrap();
+        //     read_fd = sys.arg2;
+        //     // println!("open a file: {}", sys.arg3);
+        // } else if sys.arg1 == read_fd {
+        //     // println!("{}", file_path);
+        //     let mut file = fs::OpenOptions::new()
+        //         .write(true)
+        //         .append(true)
+        //         .open(file_path.as_str())
+        //         .unwrap();
+        //     file.write(sys.arg3.as_bytes()).unwrap();
+        // }
+    }
+}
+
+fn buf_analyse(buf: String, threshold: bool) -> bool {
+    if buf.len() <= 1 {
         return true;
     } else {
         let mut tmp_file = fs::File::create("files/tmp").unwrap();
-        tmp_file.write(sys.buf.as_bytes()).unwrap();
+        tmp_file.write(buf.as_bytes()).unwrap();
 
         let result = Command::new("/usr/bin/ent")
             .args(["-t", "files/tmp"])
@@ -115,14 +197,4 @@ fn get_chi_square(result: Output) -> Value {
     value
 }
 
-fn install_ent() {
-    let ent_file = Path::new("/usr/bin/ent");
-    if !ent_file.exists() {
-        sudo::escalate_if_needed().expect("Failed to sudo");
-        let _install_ent = Command::new("sh")
-            .current_dir("source_files")
-            .arg("install_ent.sh")
-            .spawn()
-            .unwrap();
-    }
-}
+
